585ca784647557befc75dad065b5a5bfa1b876ea
EylemGokdemir05 <41906907+EylemGokdemir05@users.noreply.github.com>
2020-07-09 13:31:28 +0300

    splash screen is ok.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native';
 import AppNavigator from './src/navigations/AppNavigator';
-import {Login, SignUp} from './src/screens';
+import {Login, SignUp,SplashScreen} from './src/screens';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => SplashScreen);
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView,View, Text,Image,ImageBackground} from 'react-native';
+import styles from './style';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>Burası Login sayfası</Text>
+    <View style={styles.pages.signUpPage.container}>
+      <ImageBackground
+        style={styles.pages.signUpPage.imageBackground}
+        source={require('../assets/background.png')}>
+        <SafeAreaView style={styles.pages.signUpPage.safeAreaView}>
+          <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+              <Image source={require('../assets/signUpLogo.png')} />
+          </View>
+          <Text style={{color:'white',fontWeight:'bold',alignSelf:'center'}}>Freebie Notes</Text>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 };
