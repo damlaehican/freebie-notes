6bf22fb90b0ab5242616df10023b7d5f361e5b57
Damla <35731884+damlaehican@users.noreply.github.com>
2020-07-11 16:14:46 +0300

    Bug Fix AppNavigator

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,8 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
 import {Login, SignUp, Splash} from '../screens';
-import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 const Stack = createStackNavigator();
@@ -11,7 +10,11 @@ const MainNavigator = () => {
     <NavigationContainer initialRouteName="Splash">
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{gestureEnabled: false}}
+        />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
