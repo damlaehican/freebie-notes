1b993b241d7a2115c3fc328367b69452e0e51df8
Damla <35731884+damlaehican@users.noreply.github.com>
2020-07-20 03:38:50 +0300

    card design completed

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,46 +1,50 @@
-import React, {useState} from 'react';
-import {View, Text, Dimensions} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
 import {Clock} from '../components/SVGR-Components';
 
 const NoteCard = (props) => {
-  const [open, setOpen] = useState(false);
   return (
     <View style={{flexDirection: 'row'}}>
-      <View
-        style={{
-          flexDirection: 'column',
-          height: 250,
-          borderRadius: 10,
-          backgroundColor: 'white',
-          width: Dimensions.get('window').width / 2.3,
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-
-          elevation: 5,
-          marginLeft: 5,
-          paddingLeft: 10,
-          marginTop: 10,
-        }}>
+      <View style={styles.container}>
         <Text style={{fontSize: 12, color: '#FF5227'}}>{props.date}</Text>
         <Text style={{fontWeight: 'bold', fontSize: 16}}>{props.title}</Text>
-        <Text style={{fontSize: 14}}>{props.icerik}</Text>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'flex-end',
-            alignItems: 'flex-end',
-            bottom: 5,
-            right: 5,
-          }}>
+        <Text style={{fontSize: 14}} numberOfLines={5}>
+          {props.icerik}
+        </Text>
+        <View style={styles.clockView}>
           <Clock fill="#FF5227" width={18} height={18} />
         </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    height: 250,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    width: Dimensions.get('window').width / 2.3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    marginLeft: 5,
+    paddingLeft: 10,
+    marginTop: 20,
+  },
+  clockView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    bottom: 5,
+    right: 5,
+  },
+});
 export default NoteCard;
--- src/screens/AddNotePage.js
+++ src/screens/AddNotePage.js
@@ -1,57 +1,49 @@
 import React, {useState, useEffect} from 'react';
 import {
   View,
-  SafeAreaView,
-  Alert,
-  Image,
-  TextInput,
-  Text,
-  Dimensions,
-  ImageBackground,
+  StyleSheet,
   TouchableOpacity,
   FlatList,
   StatusBar,
 } from 'react-native';
-import {MyInput, NoteSearchBar, NoteCard} from '../components';
-import {
-  Album,
-  Plus,
-  Microphone,
-  Light,
-  Menu,
-} from '../components/SVGR-Components/';
+import {NoteSearchBar, NoteCard} from '../components';
+import {Plus} from '../components/SVGR-Components/';
 import PhotoNote from './PhotoNote';
 
 const dummy = [
   {
     time: '15.07.2020',
     title: 'Alışveriş',
-    icerik: 'Zeytin, Şarap, Peynir',
+    icerik:
+      'Eiusmod qui officia ad est.Ipsum laborum consectetur enim laboris.Laboris nisi incididunt eiusmod voluptate tempor et aute velit.',
   },
   {
     time: 'Bugün',
     title: 'Notlarım',
-    icerik: 'React Native Çalış,',
+    icerik:
+      'Pariatur sunt veniam dolore quis fugiat ipsum voluptate fugiat pariatur ipsum adipisicing exercitation cillum.',
   },
   {
     time: 'Bugün',
     title: 'Notlarım',
-    icerik: 'React Native Çalış,',
+    icerik: 'Pariatur aliqua dolor consequat et sunt laboris et labore.',
   },
   {
     time: 'Bugün',
     title: 'Notlarım',
-    icerik: 'React Native Çalış,',
+    icerik:
+      'Id amet ullamco laboris veniam irure sint cillum proident cillum ex proident.In deserunt qui esse labore eu anim reprehenderit mollit commodo tempor do commodo duis.Nulla nulla aute aute consectetur tempor.Enim nostrud tempor quis consectetur consequat labore laboris aliqua.',
   },
   {
     time: 'Bugün',
     title: 'Notlarım',
-    icerik: 'React Native Çalış,',
+    icerik: 'Elit id proident proident cillum qui sint ad voluptate eu.',
   },
   {
     time: 'Bugün',
     title: 'Notlarım',
-    icerik: 'React Native Çalış,',
+    icerik:
+      'Irure nulla ex commodo qui consectetur sit nostrud anim occaecat enim cupidatat adipisicing aliquip.',
   },
 ];
 
@@ -66,39 +58,47 @@ const AddNotePage = (props) => {
   const renderItem = ({item}) => {
     return (
       <View style={{marginRight: 10, marginLeft: 10}}>
-        <NoteCard title={item.title} icerik={item.icerik} date={item.time} />
+        <TouchableOpacity>
+          <NoteCard title={item.title} icerik={item.icerik} date={item.time} />
+        </TouchableOpacity>
       </View>
     );
   };
-
+  const split = () => {
+    let icerik = data.map((item, key) => item.icerik);
+    return icerik.slice(0, 1) + '...';
+  };
   return (
     <View style={{flex: 1}}>
       <StatusBar barStyle="light-content" />
       <NoteSearchBar />
       <FlatList data={data} numColumns={2} renderItem={renderItem} />
-      <TouchableOpacity
-        style={{
-          borderRadius: 100,
-          marginRight: 10,
-          position: 'absolute',
-          bottom: 20,
-          right: 20,
-          backgroundColor: '#ff5227',
-          width: 60,
-          height: 60,
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-
-          elevation: 5,
-        }}>
+      <TouchableOpacity style={styles.addButton}>
         <Plus width={40} height={40} fill="white" margin={10} />
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  addButton: {
+    borderRadius: 100,
+    marginRight: 10,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#ff5227',
+    width: 60,
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
 export default AddNotePage;
