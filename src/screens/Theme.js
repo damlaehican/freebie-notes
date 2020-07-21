9a6e35c068d275a64f1f3c27af6a2a6fd5d77639
EylemGokdemir05 <41906907+EylemGokdemir05@users.noreply.github.com>
2020-07-21 18:34:04 +0300

    Drawer edited.

---
+++ src/screens/Theme.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import {Switch} from 'react-native-paper'
+
+const Theme = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <View>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
+}
+
+export default Theme;
--- src/screens/index.js
+++ src/screens/index.js
@@ -5,4 +5,5 @@ export {default as NotesList} from './NotesList';
 export {default as PhotoNote} from './PhotoNote';
 export {default as VoiceNote} from './VoiceNote';
 export {default as AddNote} from './AddNote';
-export {default as Favourites} from './Favourites'
+export {default as Favourites} from './Favourites';
+export {default as Theme} from './Theme';
